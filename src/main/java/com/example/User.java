fc1aab3922c6620da568b8cb3e0dc5613ce3f36d
dhslrl321 <dhslrl321@gmail.com>
2022-07-23 10:06:29 +0900

    commit

---
+++ src/main/java/com/example/User.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class User {
+    private final String name;
+    private final int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    Main main;
+
+    @Test
+    void name() {
+        assertThat(1).isEqualTo(1);
+    }
+}
