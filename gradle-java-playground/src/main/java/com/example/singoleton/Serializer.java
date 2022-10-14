919786d521d814ce85425625a8caa1be6cae5eba
dhslrl321 <dhslrl321@gmail.com>
2022-10-14 15:33:51 +0900

    commit

---
+++ gradle-java-playground/src/main/java/com/example/singoleton/Serializer.java
@@ -0,0 +1,15 @@
+package com.example.singoleton;
+
+public class Serializer {
+    private static final Serializer instance = new Serializer();
+
+    private Serializer() {}
+
+    public static Serializer getInstance() {
+        return instance;
+    }
+
+    public void serialize(String provided) {
+        System.out.println("provided = " + provided);
+    }
+}
