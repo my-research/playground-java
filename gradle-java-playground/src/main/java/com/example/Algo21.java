21416e8f853cdd731a49ed3a31d99da397c32ef0
dhslrl321 <dhslrl321@gmail.com>
2022-12-20 13:03:09 +0900

    commit

--- gradle-java-playground/src/main/java/com/example/Algo21.java
+++ gradle-java-playground/src/main/java/com/example/Algo21.java
@@ -1,27 +1,15 @@
 package com.example;
 
+import java.net.HttpURLConnection;
+import java.net.http.HttpClient;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Algo21 {
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 4, 4, 5, 5};
-        boolean solution = solution(a, 6);
-
-        System.out.println("solution = " + solution);
-    }
-
-    private static boolean solution(int[] A, int K) {
-        int n = A.length;
-        for (int i = 0; i < n - 1; i++) {
-            if (A[i] + 1 < A[i + 1]) // 그 현재보다 1이상 크면
-                return false;
-
-            if ((A[i] != A[i+1]) && (A[i] + 1 != A[i + 1])) {
-                return false;
-            }
-        }
-        if (A[0] != 1 && A[n - 1] != K) // 두번쨰부터 &&
-            return false;
-        else
-            return true;
+        ExecutorService executor = Executors.newFixedThreadPool(10);
     }
 }
---
+++ gradle-java-playground/src/main/java/com/example/T2.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class T2 {
+    static ArrayList<Integer> a;
+    static {
+        System.out.println("created");
+        a = new ArrayList<>();
+    }
+
+}
